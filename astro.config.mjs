4edfebf05acfed594a2acc71028db796f3e1e1d0
marcolamberti66 <marco.lamberti66@gmail.com>
2026-04-22 02:59:48 +0200

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
+
+// Se siamo sui server di Netlify, questa variabile sarà vera.
+// Altrimenti (es. su StackBlitz), useremo l'ambiente Node locale che non dà errori di rete.
+const isNetlify = process.env.NETLIFY;
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: isNetlify ? netlify() : node({ mode: 'standalone' }),
   integrations: [react()],
 });
